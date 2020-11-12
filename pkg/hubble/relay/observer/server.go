59c7b835082ef74ced5f8f9e1931c3daa3ed128d
Robin Hahling <robin.hahling@gw-computing.net>
2020-11-12 14:34:48 +0100

    hubble/relay: implement observer.GetNodes rpc endpoint
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- pkg/hubble/relay/observer/server.go
+++ pkg/hubble/relay/observer/server.go
@@ -26,8 +26,7 @@ import (
 	"github.com/golang/protobuf/ptypes/wrappers"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sync/errgroup"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/metadata"
 )
 
 // numUnavailableNodesReportMax represents the maximum number of unavailable
@@ -166,7 +165,57 @@ sortedFlowsLoop:
 
 // GetNodes implements observerpb.ObserverClient.GetNodes.
 func (s *Server) GetNodes(ctx context.Context, req *observerpb.GetNodesRequest) (*observerpb.GetNodesResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "GetNodes not implemented")
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		ctx = metadata.NewOutgoingContext(ctx, md)
+	}
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	g, ctx := errgroup.WithContext(ctx)
+
+	peers := s.peers.List()
+	nodes := make([]*observerpb.Node, 0, len(peers))
+	for _, p := range s.peers.List() {
+		p := p
+		n := &observerpb.Node{
+			Name: p.Name,
+			Tls: &observerpb.TLS{
+				Enabled:    p.TLSEnabled,
+				ServerName: p.TLSServerName,
+			},
+		}
+		if p.Address != nil {
+			n.Address = p.Address.String()
+		}
+		nodes = append(nodes, n)
+		if !isAvailable(p.Conn) {
+			n.State = relaypb.NodeState_NODE_UNAVAILABLE
+			s.opts.log.WithField("address", p.Address).Infof(
+				"No connection to peer %s, skipping", p.Name,
+			)
+			s.peers.ReportOffline(p.Name)
+			continue
+		}
+		n.State = relaypb.NodeState_NODE_CONNECTED
+		g.Go(func() error {
+			n := n
+			client := s.opts.ocb.observerClient(&p)
+			status, err := client.ServerStatus(ctx, &observerpb.ServerStatusRequest{})
+			if err != nil {
+				n.State = relaypb.NodeState_NODE_ERROR
+				s.opts.log.WithFields(logrus.Fields{
+					"error": err,
+					"peer":  p,
+				}).Warning("Failed to retrieve server status")
+				return nil
+			}
+			n.Version = status.GetVersion()
+			return nil
+		})
+	}
+	if err := g.Wait(); err != nil {
+		return nil, err
+	}
+	return &observerpb.GetNodesResponse{Nodes: nodes}, nil
 }
 
 // ServerStatus implements observerpb.ObserverServer.ServerStatus by aggregating
